4048e788475b75682049aac96050754b2f6d3591
likairuo <likairuo@xiaohongshu.com>
2023-06-27 16:56:42 +0800

    first commit

---
+++ src/main/java/com/music/rica/RicaApplication.java
@@ -0,0 +1,13 @@
+package com.music.rica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/music/rica/RicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.rica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
